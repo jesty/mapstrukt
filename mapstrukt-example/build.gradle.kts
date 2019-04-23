48014c305aedc4987ce0b26f4fd902628de98ff5
Davide Cerbo <davidecerbo@gmail.com>
2019-04-23 22:47:14 +0200

    moved folder

---
+++ mapstrukt-example/build.gradle.kts
@@ -0,0 +1,33 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.2.71"
+    kotlin("kapt") version "1.2.71"
+}
+
+group = "org.mapstruct.examples"
+version = "1.0.0-SNAPSHOT"
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+
+dependencies {
+    compile(kotlin("stdlib-jdk8"))
+    compile("org.mapstruct:mapstruct:1.3.0.Final")
+    kapt("org.mapstruct:mapstruct-processor:1.3.0.Final")
+    kapt("com.github.jesty:mapstrukt:1.0.0-SNAPSHOT")
+
+    testCompile("org.junit.jupiter:junit-jupiter-api:5.3.1")
+    testCompile("org.junit.jupiter:junit-jupiter-engine:5.3.1")
+    testCompile("org.assertj:assertj-core:3.11.1")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+val test by tasks.getting(Test::class) {
+    useJUnitPlatform { }
+}
